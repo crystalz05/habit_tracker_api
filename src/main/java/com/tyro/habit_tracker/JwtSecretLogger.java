f68f96d05599d0c74c4489d43240835204348408
crystalz05 <mikecrystalz05@gmail.com>
2025-08-11 20:45:15 +0100

    update

--- src/main/java/com/tyro/habit_tracker/JwtSecretLogger.java
+++ src/main/java/com/tyro/habit_tracker/JwtSecretLogger.java
@@ -1,24 +1,19 @@
 package com.tyro.habit_tracker;
 
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JwtSecretLogger implements CommandLineRunner {
-
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-    
-	@Value("${jwt.expiration}")
-	private Long jwtExpiration;
-    
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("JWT_SECRET: " + jwtSecret);
-        System.out.println("JWT_EXPIRATION: " + jwtExpiration);
-        
-    }
-}
+//@Component
+//public class JwtSecretLogger implements CommandLineRunner {
+//
+//    @Value("${jwt.secret}")
+//    private String jwtSecret;
+//    
+//	@Value("${jwt.expiration}")
+//	private Long jwtExpiration;
+//    
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("JWT_SECRET: " + jwtSecret);
+//        System.out.println("JWT_EXPIRATION: " + jwtExpiration);
+//        
+//    }
+//}
